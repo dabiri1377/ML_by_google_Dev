6adb21ee327aff03e6dec0054d5860557bd7834b
moh <dabiri1377@gmail.com>
2018-02-09 01:23:05 +0330

    add some comment into "first_program"

--- #1/first_program.py
+++ #1/first_program.py
@@ -5,7 +5,11 @@ features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 # "3" for "apple" and "4" for "orange"
 labels = [3, 3, 4, 4]
 
+# define a Decision Tree Classifier
 clf = tree.DecisionTreeClassifier()
+
+# learn "Fine Pattern of Data" into classifier
 clf = clf.fit(features, labels)
 
-print(clf.predict([[150,0]]))
+# print kind of fruit
+print(clf.predict([[140, 0]]))
