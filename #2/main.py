1fbc98f118d8878562d0d75a8b8816f614a8b40a
moh <dabiri1377@gmail.com>
2018-02-09 03:42:29 +0330

    add more code and try to fix error in #2 main

--- #2/main.py
+++ #2/main.py
@@ -1,4 +1,8 @@
+from tkinter import Image
+
 from sklearn.datasets import load_iris
+import numpy as np
+from sklearn import tree
 
 iris = load_iris()
 # print info about dataset
@@ -8,4 +12,34 @@ print(iris.DESCR)
 for i in range(len(iris.target)):
     print("Ex %d: label %s, feature %s" % (i, iris.target[i], iris.data[i]))
 
+test_idx = [0, 50, 100]
 # training data
+train_target = np.delete(iris.target, test_idx)
+train_data = np.delete(iris.data, test_idx, axis=0)
+
+# testing data
+test_target = iris.target[test_idx]
+test_data = iris.data[test_idx]
+
+# create a DecisionTreeClassifier
+clf = tree.DecisionTreeClassifier()
+
+# train classifier
+clf.fit(train_data, train_target)
+
+print("test target = %s" % test_target)
+print(clf.predict(test_data))
+
+# viz code
+from sklearn.externals.six import StringIO
+import pydot
+
+dot_data = StringIO()
+tree.export_graphviz(clf,
+                     out_file=dot_data,
+                     class_names=iris.target_names,
+                     filled=True, rounded=True,
+                     impurity=False)
+
+graph = pydot.graph_from_dot_data(dot_data.getvalue())
+tree.export_graphviz(clf,out_file='tree.dot')
